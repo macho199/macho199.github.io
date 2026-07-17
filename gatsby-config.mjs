fb8275b37354afaa06b5746a0e2ac7f0798ccb52
kjs <macho199@gmail.com>
2026-07-17 15:52:14 +0900

    feat: bootstrap Gatsby 5 project (#2)

---
+++ gatsby-config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("gatsby").GatsbyConfig} */
+const config = {
+  siteMetadata: {
+    title: "Developer Blog",
+    siteUrl: "https://macho199.github.io",
+  },
+}
+
+export default config
